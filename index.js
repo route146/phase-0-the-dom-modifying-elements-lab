4ae203521fef2a37e8259b81b4c254534f52aa41
Micha <mlcheatham92@gmail.com>
2022-12-12 10:21:19 -0500

    commit tree f24d2c16397c2cb89398f67f1cbaa8b454960170

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const removeMe = document.getElementById("main");
+removeMe.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "michael is the champion";
